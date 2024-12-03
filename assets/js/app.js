acbd80191bd1a25e2015e0b1f5fbac7d2ac927f3
Vladi Bilonenko <bilonenko.v@gmail.com>
2024-12-03 22:47:12 +0100

    Add URL parameter handling for campaign tracking and update content in CARTO page

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,36 @@ clipboard.on('error', function(e) {
     console.error('Trigger:', e.trigger);
 });
 /* eslint-enable */
+
+// code which reads ?ref=ads-c-* or ?utm_campaign= from entry URL and adds utm_campaign= to all all links on the page
+(function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var ref = urlParams.get('ref');
+    var utm_campaign = urlParams.get('utm_campaign');
+    if (ref || utm_campaign) {
+        var links = document.querySelectorAll('a');
+        links.forEach(function(link) {
+            var href = link.getAttribute('href');
+            if (href) {
+                if (ref) {
+                    if (href.indexOf('?') === -1) {
+                        href += '?';
+                    } else {
+                        href += '&';
+                    }
+                    href += 'utm_campaign=' + ref;
+                }
+                if (utm_campaign) {
+                    if (href.indexOf('?') === -1) {
+                        href += '?';
+                    } else {
+                        href += '&';
+                    }
+                    href += 'utm_campaign=' + utm_campaign;
+                }
+                link.setAttribute('href', href);
+            }
+        });
+    }
+}
+)();
